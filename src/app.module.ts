fce580175140c0bccfd543472d2b8802455555d1
Stan Konnov <stanis.konnov@gmail.com>
2025-08-08 17:08:06 +0200

    bugfix: DI fix and simple write end-to-end.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { PostsModule } from '@src/posts/posts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot()],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot(), PostsModule],
 })
 export class AppModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from '@src/posts/posts.service';
 import { PostsController } from '@src/posts/posts.controller';
+import { DatabaseModule } from '@src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
