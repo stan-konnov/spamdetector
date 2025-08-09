2e6577eb6240a4621b6cf233c6604b1a41a828df
Stan Konnov <stanis.konnov@gmail.com>
2025-08-09 10:47:42 +0200

    chore: Swap a constant to make API more flexible to change.

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,1 @@
-export const API_VERSION = '1';
+export const API_VERSION_1 = '1';
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Version } from '@nestjs/common';
 import { Post as PostModel } from '@prisma/client';
 
-import { API_VERSION } from '@src/common/constants';
+import { API_VERSION_1 } from '@src/common/constants';
 import { PostsService } from '@src/posts/posts.service';
 import { DataApiResponseDto } from '@src/common/dtos/data.api.response';
 import { CreatePostRequestDto } from '@src/posts/dtos/create.post.request';
@@ -11,7 +11,7 @@ export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
-  @Version(API_VERSION)
+  @Version(API_VERSION_1)
   async createPost(@Body() dto: CreatePostRequestDto): Promise<DataApiResponseDto<PostModel>> {
     const createdPost = await this.postsService.createPost(dto.content);
 
