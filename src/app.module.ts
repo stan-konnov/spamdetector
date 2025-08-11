3a08241db0f05fed17951695d1e6bce5cd25a812
Stan Konnov <stanis.konnov@gmail.com>
2025-08-11 15:21:30 +0200

    chore: Move away from scheduling and onto queuing; plug in necessary modules. Update environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bullmq';
 import { PostsModule } from '@src/posts/posts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ScheduleModule.forRoot(), PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        connection: { url: config.get<string>('REDIS_URL') },
+      }),
+      inject: [ConfigService],
+    }),
+    BullModule.registerQueueAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        name: config.get<string>('QUEUE_NAME'),
+      }),
+    }),
+    PostsModule,
+  ],
 })
 export class AppModule {}
